934b1f97e1c2858a5819ffa14b6add1af8ae4cc6
Leo <leonelherman22@gmail.com>
2024-06-17 13:57:25 +0300

    --amend

--- pharma/pharma/settings.py
+++ pharma/pharma/settings.py
@@ -177,7 +177,7 @@ import os
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 # Répertoire où collecter les fichiers statiques pour le déploiement
 if not DEBUG:
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
