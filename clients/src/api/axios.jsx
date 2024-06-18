ee21c9a1bb0dbd6257ad8b1f76a9949bdf7d7ca3
Leo <leonelherman22@gmail.com>
2024-06-18 08:35:13 +0300

    resolve conflit et merge develop V1

--- clients/src/api/axios.jsx
+++
@@ -1,7 +0,0 @@
-/* eslint-disable react-refresh/only-export-components */
-import axios from "axios";
-
-export default axios.create({
-	baseURL: "http://localhost:8000/api",
-	withCredentials: true,
-});
